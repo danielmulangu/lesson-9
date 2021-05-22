b511353aae56c9e41acf6210b82ab2043b34d41a
danielmulangu <76424137+danielmulangu@users.noreply.github.com>
2021-05-22 14:15:07 +0300

    Initial Commit

---
+++ lesson 9/lesson_9App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_9App.swift
+//  lesson 9
+//
+//  Created by Daniel Mulangu on 5/22/21.
+//
+
+import SwiftUI
+
+@main
+struct lesson_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
